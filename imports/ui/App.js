a99e9764b51d321cbb0dfa4897827e6d19cc157e
Bibiana Gamba S <b.gamba10@uniandes.edu.co>
2018-11-17 17:46:09 -0500

    passing parameters

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -13,6 +13,65 @@ import SilverMeal from './ResultadosSilverMeal'
 // App component - represents the whole app
 export default class App extends Component {
 
+  constructor(props) {
+      super(props);
+      this.state = {
+        ltSopa: 0,
+        cuSopa: 0,
+        ltArroz: 0,
+        cuArroz: 0,
+        ltCarne: 0,
+        cuCarne: 0,
+        ltAgua: 0,
+        cuAgua: 0,
+        ltYogurt: 0,
+        cuYogurt: 0,
+        ltGelatina: 0,
+        cuGelatina: 0,
+        ltTomate: 0,
+        cuTomate: 0,
+        ltSalmon: 0,
+        cuSalmon: 0,
+        ltVerduras: 0,
+        cuVerduras: 0,
+        ltManzana: 0,
+        cuManzana: 0,
+        ltBrocoli: 0,
+        cuBrocoli: 0,
+        ltHuevo: 0,
+        cuHuevo: 0,
+        ltAguacate: 0,
+        cuAguacate: 0,
+        ltFrutosSecos: 0,
+        cuFrutosSecos: 0,
+        ltPasta: 0,
+        cuPasta: 0,
+        ltCereal: 0,
+        cuCereal: 0,
+        ltLeche: 0,
+        cuLeche: 0,
+        ltPollo: 0,
+        cuPollo: 0,
+        ltCompota: 0,
+        cuCompota: 0,
+        costoFijoPedido: 0,
+        tasaMantener: 0,
+        ssEnero: 0,
+        ssFebrero: 0,
+        ssMarzo: 0,
+        ssAbril: 0,
+        ssMayo: 0,
+        ssJunio: 0
+      }
+    }
+
+
+  onUpdate = (name, val) => {
+    this.setState({
+      [name]: val
+    });
+  }
+
   render() {
     return (
       <div className="container">
@@ -28,13 +87,12 @@ export default class App extends Component {
               <div className="tab-padding">
                 <div className = "row">
                   <div className = "col-sm-8">
-                    <LeadTime></LeadTime>
-
+                    <LeadTime onUpdate={this.onUpdate}></LeadTime>
                   </div>
                   <div className = "col-sm-4">
-                    <Costos></Costos>
+                    <Costos onUpdate={this.onUpdate}></Costos>
                     <hr/>
-                    <SecurityStock></SecurityStock>
+                    <SecurityStock onUpdate={this.onUpdate}></SecurityStock>
 
                   </div>
 
@@ -54,7 +112,7 @@ export default class App extends Component {
 
             <Tab label="Silver Meal">
               <div className="tab-padding">
-                <SilverMeal></SilverMeal>
+                <SilverMeal passState={this.state}></SilverMeal>
               </div>
             </Tab>
 
@@ -68,7 +126,9 @@ export default class App extends Component {
         </Tabs>
 
 
-
+        <footer>
+        <p>© Made with love by Grupo 35</p>
+        </footer>
 
 
       </div>
